25874d6c459039982b7ea04e0496804c93f236c1
Karen <karenpenglabs@gmail.com>
2014-07-24 23:25:47 -0400

    async

--- fsFilter.js
+++ fsFilter.js
@@ -2,32 +2,18 @@ var fs = require('fs')
 var path = require('path')
 
 module.exports = function (fileDir, extName, callback) {
-		if (err) callback(err)
 
-<<<<<<< HEAD
-fs.readdir(fileDir,function(err, list){
-	//console.log(list)
-	if(err){
-		return callback(err)
-	}
-
-	var result = list.filter(function (file){
-		return path.extname(file) === '.'+ extName
-	})
-	callback(null, result)
-
-})
-
-=======
-		fs.readdir(fileDir, function (err, files) {
-				//console.log(files)
-				for (var i = 0; i < files.length; i++) {
-						if (path.extname(files[i]) === '.' + extName) {
-								//console.log(files[i])
-								return files[i]
-						}
+		fs.readdir(fileDir, function (err, list) {
+				//console.log(list)
+				if (err) {
+						return callback(err)
 				}
+
+				var result = list.filter(function (file) {
+						return path.extname(file) === '.' + extName
+				})
+				callback(null, result)
+
 		})
-		callback(null, data);
->>>>>>> 903dfb41efee6543dff27be8db9624e2e3dcc7c9
+
 }
--- program.js
+++ program.js
@@ -1,33 +1,20 @@
 // var sum = 0
 
-<<<<<<< HEAD
 // for(var i = 2; i < process.argv.length; i++){
 // 	sum += Number(process.argv[i])
 // }
-
-
 
 var fs = require('fs')
 var path = require('path')
 var http = require('http')
 var bl = require('bl')
-
-//var fsFilter = require('./fsFilter')
-=======
-for (var i = 2; i < process.argv.length; i++) {
-  sum += Number(process.argv[i])
-}
-
-// var fs = require('fs')
-// var path = require('path')
-var fsFilter = require('./fsFilter')
->>>>>>> 903dfb41efee6543dff27be8db9624e2e3dcc7c9
+var async = require('async')
 
 //console.log(fs.readFileSync(process.argv[2]).toString().split('\n').length -1 )
 
 // fs.readFile(process.argv[2],function(err, data){
 // 	console.log(data.toString().split('\n').length -1 );
-<<<<<<< HEAD
+
 //  }) console.log(process.argv[2])
 
 // fs.readdir('../learnyounode', function(err, files){
@@ -39,51 +26,103 @@ var fsFilter = require('./fsFilter')
 // 			console.log(files[i])
 // 		}
 // 	}
-//  }) 
-
-// var dir = process.argv[2]
-// var filterStr = process.argv[3]
-
-
-// fsFilter(dir, filterStr, function(err, list){
-
-// 	if(err)
-// 		return console.log("err")
-
-// 	list.forEach(function(file){
-// 		console.log(file)
-// 	})
-
-// })
-var url = process.argv[2]
-//console.log(process.argv[2])
-
-http.get(url, function(res){
-	res.setEncoding('utf8');
-	res.pipe(bl(function(err,data){
-		//data.setEncoding('utf8')
-		console.log(data.toString('utf8').length);
-		console.log(data.toString('utf8'));
-	}));
-	// res.on('data',function(data){
-	// 	//console.log(data.toString().length);
-	// 	console.log(data);
-	// })
-})
-=======
 //  })
 
-//console.log(process.argv[2])
-// fs.readdir(process.argv[2], function (err, files) {
-//   //console.log(files)
-//   for (var i = 0; i < files.length; i++) {
-//     if (path.extname(files[i]) === '.' + process.argv[3]) {
-//       console.log(files[i])
-//     }
-//   }
-// })
+/*
+var fsFilter = require('./fsFilter')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
 
-fsFilter(process.argv[2], process.argv[3], function () {
+fsFilter(dir, filterStr, function (err, list) {
 
-});
->>>>>>> 903dfb41efee6543dff27be8db9624e2e3dcc7c9
+		if (err)
+				return console.log("err")
+
+		list.forEach(function (file) {
+				console.log(file)
+		})
+
+})
+*/
+
+/*
+var url = process.argv[2];
+http.get(url, function (res) {
+		res.setEncoding('utf8')
+		res.on('data', function (data) {
+				console.log(data)
+		})
+		res.on('error', function (error) {
+				console.log("error")
+		})
+})
+*/
+
+/*
+
+var url = process.argv[2]
+		//console.log(process.argv[2])
+
+http.get(url, function (res) {
+		res.setEncoding('utf8');
+		res.pipe(bl(function (err, data) {
+				//data.setEncoding('utf8')
+				console.log(data.toString('utf8').length);
+				console.log(data.toString('utf8'));
+		}));
+		// res.on('data',function(data){
+		// 	//console.log(data.toString().length);
+		// 	console.log(data);
+		// })
+})
+
+*/
+
+var url = process.argv[2]
+		//console.log(process.argv[2])
+var result = [];
+//var i = 0;
+function getData(i, total) {
+		http.get(process.argv[2 + i], function (res) {
+				res.setEncoding('utf8');
+				res.pipe(bl(function (err, data) {
+						//data.setEncoding('utf8')
+						//console.log(data.toString('utf8').length);
+						//console.log(data.toString('utf8'));
+						//return true;
+						//result.push(data.toString('utf8'));
+						console.log(data.toString('utf8'));
+						i++;
+						if (i < total) {
+								//console.log(data.toString('utf8'))
+								getData(i);
+						} else {
+								for (var j = 0; j < result.length; j++) {
+										//	console.log(result[j])
+								}
+						}
+
+				}));
+		})
+}
+getData(0, 3);
+// //async.series([
+// function queup() {
+// 		if (getData(process.argv[2])) {
+// 				if (getData(process.argv[3])) {
+// 						getData(process.argv[4])
+// 				}
+// 		}
+
+// }
+// //])
+
+// async.series([
+// 		getData(process.argv[2]),
+// 		getData(process.argv[3]),
+// 		getData(process.argv[4])
+// ]);
+
+//console.log(result)
+
+// queup();
